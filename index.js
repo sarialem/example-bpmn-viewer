4116c0c07464377f6d65b9fcad19381ac9d3b5a3
sari.alalem <sari.alalem@healthy.io>
2023-02-23 15:31:44 +0200

    Add Save as SVG and as XML (BPMN)

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const fs = require('fs');
 http.createServer((function(rqeuest, response) {
     if(rqeuest.url ==='/') {
         fs.readFile('./bpmn.html', (error, data) => {
-            console.log(error);
             response.writeHeader(200, {'Content-Type': 'text/html'})
             response.write(data);
             response.end();
         });
     }
     if(rqeuest.url ==='/bpmn.xml') {
-        fs.readFile('./bpmn2.xml', async (error, data) => {
+        fs.readFile('./bpmn2.bpmn', async (error, data) => {
             const xml = data.toString();
             response.writeHeader(200, {'Content-Type': 'text/xml'})
             response.write(xml);
